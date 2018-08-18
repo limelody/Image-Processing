2ba4e94c622106aee7a31605f597528d8204673b
unknown <li.melody1999@gmail.com>
2018-08-18 12:35:58 -0400

    Renamed variable and display only RBG

--- processing.js
+++ processing.js
@@ -2,19 +2,20 @@ var img = new Image();
 img.src = 'assets/rnm.png';
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+
 img.onload = function() {
   ctx.drawImage(img, 0, 0);
   img.style.display = 'none';
 };
-var color = document.getElementById('color');
+
+var color_box = document.getElementById('color_box');
 function pick(event) {
   var x = event.layerX;
   var y = event.layerY;
   var pixel = ctx.getImageData(x, y, 1, 1);
   var data = pixel.data;
-  var rgba = 'rgba(' + data[0] + ', ' + data[1] +
-             ', ' + data[2] + ', ' + (data[3] / 255) + ')';
-  color.style.background =  rgba;
-  color.textContent = rgba;
+  var rgb = 'rgb(' + data[0] + ', ' + data[1] + ', ' + data[2] + ')';
+  color_box.style.background = rgb;
+  color_box.textContent = rgb;
 }
 canvas.addEventListener('mousemove', pick);
